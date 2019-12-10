000422b72bdff85ed47c046a47f5952806ba3129
jacobt2753 <jacobat2@illinois.edu>
2019-12-10 00:11:53 -0600

    12:10am 12/10

--- app/src/main/java/com/example/mp5/MainActivity.java
+++ app/src/main/java/com/example/mp5/MainActivity.java
@@ -7,20 +7,22 @@ import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
+import android.view.View.OnClickListener;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.view.View.OnClickListener;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button next = (Button) findViewById(R.id.button6);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -42,4 +44,5 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
--- app/src/main/java/com/example/mp5/doSearch.java
+++ app/src/main/java/com/example/mp5/doSearch.java
@@ -2,44 +2,45 @@ package com.example.mp5;
 import android.os.AsyncTask;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RadioGroup;
 import android.view.ViewGroup.LayoutParams;
 
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
+import androidx.appcompat.app.AppCompatActivity;
+public abstract class doSearch extends AppCompatActivity {
+    public class Searchdo extends AsyncTask<Void, Void, String> {
+        Response response;
+        private Exception exception;
+        protected void onPreExecute() {
 
-
-public class doSearch extends AsyncTask<Void, Void, String> {
-    Response response;
-    private Exception exception;
-    protected void onPreExecute() {
-       
-
-    }
-    protected String doInBackground(Void... urls) {
-        try {
-            OkHttpClient client = new OkHttpClient();
-
-            Request request = new Request.Builder()
-                    .url("https://microsoft-azure-bing-news-search-v1.p.rapidapi.com/search?q=wow")
-                    .get()
-                    .addHeader("x-rapidapi-host", "microsoft-azure-bing-news-search-v1.p.rapidapi.com")
-                    .addHeader("x-rapidapi-key", "f572830147msh03231405d4e5c5bp13eef3jsn04670819f50a")
-                    .build();
-
-            Response response = client.newCall(request).execute();
         }
-        catch (Exception e) {
-            Log.e("Error", exception.getMessage(), exception);
-            return null;
+        protected String doInBackground(Void... urls) {
+            try {
+                OkHttpClient client = new OkHttpClient();
+
+                Request request = new Request.Builder()
+                        .url("https://microsoft-azure-bing-news-search-v1.p.rapidapi.com/search?q=wow")
+                        .get()
+                        .addHeader("x-rapidapi-host", "microsoft-azure-bing-news-search-v1.p.rapidapi.com")
+                        .addHeader("x-rapidapi-key", "f572830147msh03231405d4e5c5bp13eef3jsn04670819f50a")
+                        .build();
+
+                Response response = client.newCall(request).execute();
+            }
+            catch (Exception e) {
+                Log.e("Error", exception.getMessage(), exception);
+                return null;
+            }
+            return response.toString();
         }
-        return response.toString();
-    }
-    protected void onPostExecute(String response) {
-        if (response == null) {
-            response = "An error has occurred.";
+        protected void onPostExecute(String response) {
+            if (response == null) {
+                response = "An error has occurred.";
+            }
+            Log.i("Feed", response);
         }
-        Log.i("Feed", response);
     }
 }
