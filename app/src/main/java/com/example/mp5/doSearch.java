f52e204ccb09e79cc5782353cd434b17cc7f7965
Jacob Thomas <jacobthomas@wirelessprv-10-195-69-241.near.illinois.edu>
2019-12-09 20:29:36 -0600

    Initial commit

---
+++ app/src/main/java/com/example/mp5/doSearch.java
@@ -0,0 +1,45 @@
+package com.example.mp5;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.view.View;
+import android.widget.RadioGroup;
+import android.view.ViewGroup.LayoutParams;
+
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+
+public class doSearch extends AsyncTask<Void, Void, String> {
+    Response response;
+    private Exception exception;
+    protected void onPreExecute() {
+       
+
+    }
+    protected String doInBackground(Void... urls) {
+        try {
+            OkHttpClient client = new OkHttpClient();
+
+            Request request = new Request.Builder()
+                    .url("https://microsoft-azure-bing-news-search-v1.p.rapidapi.com/search?q=wow")
+                    .get()
+                    .addHeader("x-rapidapi-host", "microsoft-azure-bing-news-search-v1.p.rapidapi.com")
+                    .addHeader("x-rapidapi-key", "f572830147msh03231405d4e5c5bp13eef3jsn04670819f50a")
+                    .build();
+
+            Response response = client.newCall(request).execute();
+        }
+        catch (Exception e) {
+            Log.e("Error", exception.getMessage(), exception);
+            return null;
+        }
+        return response.toString();
+    }
+    protected void onPostExecute(String response) {
+        if (response == null) {
+            response = "An error has occurred.";
+        }
+        Log.i("Feed", response);
+    }
+}
